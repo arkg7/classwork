96b02a0ecf0accb915feaf9a12db603ee37ec36b
arkg7 <archergames7@gmail.com>
2025-02-20 20:00:57 -0600

    fix progs

---
+++ cs3345/infix.java
@@ -0,0 +1,46 @@
+package cs3345;
+import java.util.Scanner;
+import java.util.Stack;
+public class infix{
+    public static void main(String[] args) {
+        Stack<String> opStack = new Stack<>();
+        String infix = "";
+        Scanner scan = new Scanner(System.in);
+        String postfix = scan.nextLine();
+        System.out.println("Postfix:"+postfix);
+        for (int i = 0; i < postfix.length(); i++) {
+            String op1;
+            String op2;
+            switch(postfix.charAt(i)){
+                case('*'):
+                    op1 = opStack.pop();
+                    op2 = opStack.pop();
+                    opStack.push("("+op2+postfix.charAt(i)+op1+")");
+                    break;
+                case('/'):
+                    op1 = opStack.pop();
+                    op2 = opStack.pop();
+                    opStack.push("("+op2+postfix.charAt(i)+op1+")");
+                    break;
+                case('+'):
+                    op1 = opStack.pop();
+                    op2 = opStack.pop();
+                    opStack.push("("+op2+postfix.charAt(i)+op1+")");
+                    break;
+                case('-'):
+                    op1 = opStack.pop();
+                    op2 = opStack.pop();
+                    opStack.push("("+op2+postfix.charAt(i)+op1+")");
+                    break;
+                default:
+                    opStack.push(postfix.substring(i,i+1));
+                    break;
+            }
+            
+        }
+        if(opStack.size()==1){
+                infix += opStack.pop();
+        }
+        System.out.println("Postfix:"+infix);
+    }
+}
--- cs3345/postfix.java
+++ cs3345/postfix.java
@@ -1,15 +1,15 @@
 package cs3345;
+import java.util.Scanner;
 import java.util.Stack;
 public class postfix{
     public static void main(String[] args) {
-        String infix = "A+(B*C+D)/E";
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter an infix expression: ");
+        String infix = scan.nextLine();
         String postfix = "";
         String popped;
         Stack<String> opStack = new Stack<>();
         for (int i = 0; i < infix.length(); i++) {
-            System.out.println("Opstack: "+opStack);
-            System.out.println("Char:"+infix.charAt(i));
-            System.out.println("Postfix:"+postfix+"\n");
             switch (infix.charAt(i)) {
                 case ('('):
                     opStack.push(infix.substring(i,i+1));
@@ -21,29 +21,34 @@ public class postfix{
                         popped = opStack.pop();
                     }   
                     break;
+                case ('^'):
+                        popped = opStack.pop();
+                        postfix += popped;
+                    opStack.push(infix.substring(i,i+1));
+                    break;    
                 case ('*'):
-                    while(!opStack.isEmpty()&&(opStack.peek().equals("/")||opStack.peek().equals("*"))){
+                    while(!opStack.isEmpty()&&(opStack.peek().equals("/")||opStack.peek().equals("*")||opStack.peek().equals("^"))){
                         popped = opStack.pop();
                         postfix += popped;
                     }
                     opStack.push(infix.substring(i,i+1));
                     break;
                 case ('/'):
-                    while(!opStack.isEmpty()&&(opStack.peek().equals("/")||opStack.peek().equals("*"))){
+                    while(!opStack.isEmpty()&&(opStack.peek().equals("/")||opStack.peek().equals("*")||opStack.peek().equals("^"))){
                         popped = opStack.pop();
                         postfix += popped;
                     }
                     opStack.push(infix.substring(i,i+1));
                     break;
                 case ('+'):
-                    while(!opStack.isEmpty()&&(opStack.peek().equals("/")||opStack.peek().equals("*")||opStack.peek().equals("+")||opStack.peek().equals("-"))){
+                    while(!opStack.isEmpty()&&(opStack.peek().equals("/")||opStack.peek().equals("*")||opStack.peek().equals("+")||opStack.peek().equals("-")||opStack.peek().equals("^"))){
                         popped = opStack.pop();
                         postfix += popped;
                     }
                     opStack.push(infix.substring(i,i+1));
                     break;
                 case ('-'):
-                    while(!opStack.isEmpty()&&(opStack.peek().equals("/")||opStack.peek().equals("*")||opStack.peek().equals("+")||opStack.peek().equals("-"))){
+                    while(!opStack.isEmpty()&&(opStack.peek().equals("/")||opStack.peek().equals("*")||opStack.peek().equals("+")||opStack.peek().equals("-")||opStack.peek().equals("^"))){
                         popped = opStack.pop();
                         postfix += popped;
                     }

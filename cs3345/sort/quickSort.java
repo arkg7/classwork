f083b54a057478fa108db35fe9c41b731908371a
arkg7 <archergames7@gmail.com>
2025-04-14 18:42:37 -0500

    quickSort

--- cs3345/sort/quickSort.java
+++ cs3345/sort/quickSort.java
@@ -1,22 +1,46 @@
 package cs3345.sort;
 //quicksort - median of three partition - cutoff 15
+
 public class quickSort {
     public static void sort(int[] arr){
-        int partIndex = partition(arr);
+        quickRecur(arr, 0, arr.length-1);
     }
-    private static int partition(int[] arr){
-        int[] med = {arr[0],arr[arr.length/2],arr[arr.length]};
-        if(med[0]>med[1]){
-            int temp = med[0];
-            med[0] = med[1];
-            med[1] = med[0];
-        }
-        int value = med[2];
-        int i = 1;
-        while(value<med[i--]){
-            med[i+1]=med[i];
-        }
-        med[i] = value;
 
+    private static void quickRecur(int[] arr, int start, int end){
+        median(arr, start, end);
+        partition(arr, start, end);
+        
+    } 
+
+    private static void median(int[] arr, int l, int r){
+        int mid = (l + r)/2;
+        if(arr[l]>=arr[mid]&&arr[0]<=arr[r]||arr[l]<=arr[mid]&&arr[l]>=arr[r]){
+            swap(arr, l, r);
+        }else if(arr[mid]>=arr[l]&&arr[mid]<=arr[r]||arr[mid]<=arr[l]&&arr[mid]>=arr[r]){
+            swap(arr, mid, r);
+        }
+    }
+
+    private static void partition(int[] arr, int l, int r){
+        int i = l;
+        int j = r-1;
+        while(i<j){
+            while(arr[i]<arr[r]){
+                i++;
+            }
+            while(arr[j]>arr[r]){
+                j--;
+            }
+            if(arr[i]>arr[j]){
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i, r);
+    }
+
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
--- cs3345/sort/test.java
+++ cs3345/sort/test.java
@@ -3,9 +3,15 @@ package cs3345.sort;
 public class test {
     public static void main(String[] args) {
         int[] arr = {11,4,6,3,6,7,8,1,3,6,10,2};
+        int[] arr2 = arr;
         mergeSort.sort(arr);
         for(int i: arr){
             System.out.print(i+" ");
         }
+        quickSort.sort(arr2);
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
+
     }
 }

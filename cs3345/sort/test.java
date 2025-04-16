7e6193326995b469640aa508818ce2b52f5503c9
arkg7 <archergames7@gmail.com>
2025-04-15 20:21:41 -0500

    3345 proj2 finish

--- cs3345/sort/test.java
+++ cs3345/sort/test.java
@@ -2,14 +2,25 @@ package cs3345.sort;
 
 public class test {
     public static void main(String[] args) {
-        int[] arr1 = {43, 2, 19, 12, 50, 27, 3, 35, 6, 41};
-        int[] arr2 = {43, 2, 19, 12, 50, 27, 3, 35, 6, 41, 24, 38, 11, 46, 22, 5, 9, 17,
-            48, 8};
-        int[] arr3 = {43, 2, 19, 12, 50, 27, 3, 35, 6, 41, 24, 38, 11, 46, 22, 5, 9, 17,
-            48, 8, 33, 45, 29, 13, 25, 21, 18, 39, 7, 30};
+        int[] arr1 = {4, 7, 1, 10, 3, 6, 9, 2, 8, 5};
+        int[] arr2 = {43, 18, 9, 27, 3, 31, 47, 39, 19, 50,
+            2, 22, 26, 33, 15, 24, 36, 44, 7, 12,
+            35, 17, 4, 13, 42, 28, 11, 32, 23, 8,
+            30, 5, 38, 6, 40, 46, 1, 10, 14, 21,
+            25, 37, 48, 20, 34, 41, 29, 16, 49, 45};
+        int[] arr3 = {81, 25, 68, 11, 70, 33, 87, 7, 93, 63,
+            55, 14, 32, 22, 47, 9, 19, 85, 53, 77,
+            73, 41, 62, 27, 18, 36, 91, 16, 20, 8,
+            42, 98, 97, 2, 6, 12, 84, 31, 29, 59,
+            65, 26, 5, 35, 67, 38, 69, 49, 17, 40,
+            61, 3, 10, 90, 71, 86, 13, 23, 43, 45,
+            94, 4, 50, 24, 21, 75, 28, 64, 66, 99,
+            58, 76, 96, 30, 39, 37, 74, 15, 44, 72,
+            34, 48, 80, 78, 46, 1, 83, 52, 79, 88,
+            89, 56, 60, 95, 92, 51, 100, 57, 54, 82};
+        speedTest(arr1);
         System.out.println("Test 1");
         long[] test1 = speedTest(arr1);
-        test1 = speedTest(arr1);
         System.out.println("Test 2");
         long[] test2 = speedTest(arr2);
         System.out.println("Test 3");
@@ -17,7 +28,7 @@ public class test {
 
         for(int i = 0; i<2; i++){
             if(i==0){
-                System.out.println("\nMergesort Time elapsed (nanoseconds):\t\t\tTest 1\t\t\tTest 2\t\t\tTest 3");
+                System.out.println("\nMergesort Time elapsed (nanoseconds):\t\t\tTest 1 (10 int)\t\tTest 2 (50 int)\t\tTest 3 (100 int)");
             }else{
                 System.out.println("\nQuicksort Time elapsed (nanoseconds):");
             }
